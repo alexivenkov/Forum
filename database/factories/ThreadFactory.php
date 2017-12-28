79a30af99bf1a9abadd2bd5c32c50554296e1225
alex <wish@mail.ru>
2017-12-29 00:43:45 +0300

    init

--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -13,11 +13,12 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\Models\User::class, function (Faker $faker) {
+$factory->define(App\Models\Thread::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'user_id' => function() {
+            return factory(\App\Models\User::class)->create()->id;
+        }
     ];
 });
